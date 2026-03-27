bedfbc4c100fd13351ff9924b2418b8f0bc61cb9
Kureykin <148269382+Kureykin@users.noreply.github.com>
2026-03-27 15:51:09 +0100

    Delete target/classes/SQLScripts directory

--- target/classes/SQLScripts/find_youngest_eldest_workers.sql
+++
@@ -1,4 +0,0 @@
-select case
-    when BIRTHDAY = (select max(BIRTHDAY ) from WORKER) then 'YOUNGEST'
-    else 'ELDEST'
-end as type, NAME, BIRTHDAY from WORKER where BIRTHDAY = (select max(BIRTHDAY ) from WORKER) or BIRTHDAY = (select min(BIRTHDAY ) from WORKER);
